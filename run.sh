310d33ea02d8b5894203a879315d3a341b8e0a19
jason <jk17@ualberta.ca>
2014-06-11 09:01:26 -0700

    update with remote option.

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
-if [ -z "$WERCKER_GIT_PUSH_REMOTE"  ]; then
+if [ -z "$WERCKER_GIT_PUSH_GITHUB_ACCESS_TOKEN"  ]; then
+    echo "pushing to default origin. You may not have permission to do this. Check if you have correctly set your access token"
     git push origin $WERCKER_GIT_BRANCH --tags
 else
-    git push $WERCKER_GIT_PUSH_REMOTE $WERCKER_GIT_BRANCH --tags
+    echo "access token is specified"
+    REMOTE="https://$WERCKER_GIT_PUSH_GITHUB_ACCESS_TOKEN@github.com/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY.git"
+    echo "pushing to remote [ $REMOTE ]"
+    git push $REMOTE $WERCKER_GIT_BRANCH --tags
 fi
 
